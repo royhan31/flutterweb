233e8d9d1ef3f8eb4ba188be2a58798be47bfb32
royhan31 <izzapark34@gmail.com>
2019-05-31 22:34:30 +0700

    push heroku

--- web/main.dart
+++ web/main.dart
@@ -1,10 +1,16 @@
-// Copyright 2019 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-import 'package:flutter_web_ui/ui.dart' as ui;
-import 'package:web/main.dart' as app;
-
-main() async {
-  await ui.webOnlyInitializePlatform();
-  app.main();
+import 'dart:io' show Platform;
+import 'dart:async' show runZoned;
+import 'package:path/path.dart' show join, dirname;
+import 'package:shelf/shelf_io.dart' as io;
+import 'package:shelf_static/shelf_static.dart';
+void main() {
+  // Assumes the server lives in bin/ and that `webdev build` ran
+  var pathToBuild = join(dirname(Platform.script.toFilePath()), '..', 'build');
+  var handler = createStaticHandler(pathToBuild, defaultDocument: 'index.html');
+  var portEnv = Platform.environment['PORT'];
+  var port = portEnv == null ? 9999 : int.parse(portEnv);
+  runZoned(() {
+    io.serve(handler, '0.0.0.0', port);
+    print("Serving $pathToBuild on port $port");
+  }, onError: (e, stackTrace) => print('Oh noes! $e $stackTrace'));
 }
